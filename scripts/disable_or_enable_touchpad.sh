e3ca16954db3481716340733d282a1d3118963de
Zhou Xianhua <xianhua.zhou@gmail.com>
2010-11-21 09:51:18 +0800

    disable touchpad by unload the psmouse module.

--- scripts/disable_or_enable_touchpad.sh
+++ scripts/disable_or_enable_touchpad.sh
@@ -6,3 +6,7 @@ xinput set-int-prop 12 "Device Enabled" 8 0
 
 # enable touchpad
 xinput set-int-prop 12 "Device Enabled" 8 1
+
+
+#### another way (my touchpad changed to psmouse?)
+modprobe -r psmouse
